25d97e2201a15a434487649dd3777630145eb2bc
do2folio <joon9817@naver.com>
2018-08-10 14:45:03 +0900

    newline replacement setting with main board

--- src/components/Board/item.jsx
+++ src/components/Board/item.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const ItemWrapper = styled.div`
   display: inline-block;
@@ -9,13 +9,14 @@ const ItemWrapper = styled.div`
   max-width: 360px;
   margin: 24px 20px;
   font-size: 14px;
+  vertical-align: top;
 `;
 
 const ItemTitle = styled.div`
   height: 96px;
   padding: 8px;
   border: solid 1px #e6e6e6;
-  border-top: solid 3px ${props => (props.bools ? '#f00' : '#5af')};
+  border-top: solid 3px ${props => (props.bools ? "#f00" : "#5af")};
 `;
 
 const ItemContent = styled.div`
@@ -25,33 +26,37 @@ const ItemContent = styled.div`
   border-top: none;
 `;
 
-const Item = ({
-  title, content, bools, onToggle,
-}) => (
-  <ItemWrapper onClick={onToggle}>
-    <ItemTitle bools={bools}>
-      {title}
-    </ItemTitle>
-    <ItemContent>
-      {content}
-    </ItemContent>
-  </ItemWrapper>
-);
+const Item = ({ title, content, bools, onToggle }) => {
+  const contentFormat = content.split("\n").map((line, idx) => {
+    return (
+      <Fragment key={idx}>
+        {line}
+        <br />
+      </Fragment>
+    );
+  });
+  return (
+    <ItemWrapper onClick={onToggle}>
+      <ItemTitle bools={bools}>{title}</ItemTitle>
+      <ItemContent>{contentFormat}</ItemContent>
+    </ItemWrapper>
+  );
+};
 
 Item.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   bools: PropTypes.bool,
-  onToggle: PropTypes.func.isRequired,
+  onToggle: PropTypes.func.isRequired
 };
 Item.defaultProps = {
-  bools: false,
+  bools: false
 };
 ItemTitle.propTypes = {
-  bools: PropTypes.bool,
+  bools: PropTypes.bool
 };
 ItemTitle.defaultProps = {
-  bools: false,
+  bools: false
 };
 
 export default Item;
